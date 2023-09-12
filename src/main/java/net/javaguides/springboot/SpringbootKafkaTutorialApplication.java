a82e1fd72178f4cb156d163f7af4d54d64f010e6
varunsss <varunsonkar03@gmail.com>
2023-09-12 12:41:07 +0530

    Add files via upload

---
+++ src/main/java/net/javaguides/springboot/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,17 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+	}
+
+}
+
+//     .\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+
+//     .\bin\windows\kafka-server-start.bat .\config\server.properties
